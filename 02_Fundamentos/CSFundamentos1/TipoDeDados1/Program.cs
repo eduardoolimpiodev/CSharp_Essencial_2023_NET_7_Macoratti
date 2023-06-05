423ba3328eefe4dd48c87098b70c9a3b8168a8aa
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-05 19:10:27 -0300

    Feature: Tipos Numéricos Integrais

---
+++ 02_Fundamentos/CSFundamentos1/TipoDeDados1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Declarando Variáveis Numéricas!");
+Console.WriteLine();
+
+//Declarando variáveis
+
+byte valor1 = 255;
+sbyte valor2 = -127;
+int valor3 = -2147483647;
+valor3 = 1000;
+uint valor4 = 2147483647;
+long valor5 = -21474836489;
+
+int var1 = 8, var2 = 10, var3 = 11;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+Console.WriteLine(var1);
+Console.WriteLine(var2);
+Console.WriteLine(var3);
+
+Console.ReadLine();
