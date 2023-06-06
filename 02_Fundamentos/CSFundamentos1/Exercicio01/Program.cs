5b90f51267a6743d7509e1ffaf6bb80e276ed247
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-06 14:11:21 -0300

    Feature: Exercício 01 do Curso

---
+++ 02_Fundamentos/CSFundamentos1/Exercicio01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO.Pipes;
+
+Console.WriteLine("## Exercício 01 ##");
+
+int idade = 35;
+Console.WriteLine($"A idade é: {idade}.");
+
+string nome = "Maria";
+Console.WriteLine($"O nome é {nome}.");
+
+float altura = 3.45f;
+Console.WriteLine($"A altura é: {altura}.");
+
+DateTime data = new DateTime(1999, 09, 04);
+Console.WriteLine(data.ToShortDateString());
+
+const int ANO = 12;
+Console.WriteLine(ANO);
+
+double? nota = 7.80d;
+Console.WriteLine(nota);
+
+int x = 77;
+int y = 66;
+int soma = x + y;
+Console.WriteLine(soma);
+
+Console.ReadLine();
