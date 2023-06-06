1fd3355b342a47e778285ea603cfab06c5c406ae
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-06 15:23:53 -0300

    Feature: FOrmatação - Sequência de Escapes

---
+++ 02_Fundamentos/CSFundamentos1/SaidaDeDados02/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Saída de dados: Usando sequencia de escapes.");
+Console.WriteLine();
+
+string local = "c:\\dados\\poesias.txt";
+
+string frase = "Ele falou: \"Não fui eu\".";
+
+string pizza = "\nPizza\nde\nMussarela";
+
+string bolo = "\nBolo\tde\tChocolate";
+
+Console.WriteLine(local);
+Console.WriteLine(frase);
+Console.WriteLine(pizza);
+Console.WriteLine(bolo);
+
+//Formatação usando sequencias Escapes ->
+//https://learn.microsoft.com/pt-br/dotnet/standard/base-types/character-escapes-in-regular-expressions
+
+Console.ReadLine();
