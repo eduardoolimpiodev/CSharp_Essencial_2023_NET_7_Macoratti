9cf3a9f1ea42cfd96048872b3b0776621b74269b
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-07 11:32:06 -0300

    Feature: Entrada de dados.

---
+++ 02_Fundamentos/CSFundamentos1/EntradaDeDados/Program.cs
@@ -0,0 +1,19 @@
+﻿// Link tabela ASCII : https://web.fe.up.pt/~ee96100/projecto/Tabela%20ascii.htm
+
+Console.WriteLine("## Entrada de dados ##\n");
+
+Console.WriteLine("\nInforme o seu nome:");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine($"\nO seu nome é: {nome}.");
+
+Console.WriteLine("\nInforme a sua idade:");
+
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"\nA sua idade é: {idade}.");
+
+
+Console.ReadKey();
+
