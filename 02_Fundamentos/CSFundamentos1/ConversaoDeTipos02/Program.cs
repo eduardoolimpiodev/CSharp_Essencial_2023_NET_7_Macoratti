ab07884ed6afd3ef0f245fd0192b002569f10c9b
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-07 11:03:04 -0300

    Feature: Conversões de tipos.

---
+++ 02_Fundamentos/CSFundamentos1/ConversaoDeTipos02/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Conversão para String usando o método ToString!\n");
+
+int valorInt = 123;
+double valorDouble = 12.45;
+decimal valorDecimal = 12.45678m;
+
+string s1 = valorInt.ToString();
+string s2 = valorDouble.ToString();
+string s3 = valorDecimal.ToString();
+
+Console.WriteLine(s1);
+Console.WriteLine(s2);
+Console.WriteLine(s3);
+
+int valorInt2 = 10;
+double valorDouble2 = 5.35;
+bool valorBool = true;
+
+Console.WriteLine(Convert.ToString(valorInt2));
+Console.WriteLine(Convert.ToDouble(valorInt2));
+Console.WriteLine(Convert.ToString(valorBool));
+Console.WriteLine(Convert.ToInt32(valorDouble2));
+
+
+Console.ReadLine();
+
+
+/* 
+ToBoolean()
+ToChar()
+ToDouble()
+ToInt16()
+ToInt32()
+ToString()
+ */
