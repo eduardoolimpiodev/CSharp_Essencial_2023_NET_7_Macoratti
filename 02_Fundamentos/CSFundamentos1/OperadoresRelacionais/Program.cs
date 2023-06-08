64384583b1553d2296949fae86b3a7e6d8937db9
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-08 17:13:12 -0300

    Feature: Operadores Relacionais.

---
+++ 02_Fundamentos/CSFundamentos1/OperadoresRelacionais/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.WriteLine("Operadores Relacionais \n");
+
+int x = 10;
+int y = 20;
+
+Console.WriteLine($"Valor de x: {x}");
+Console.WriteLine($"Valor de y: {y}");
+
+bool resultado = x == y;
+Console.WriteLine(resultado);
+
+
+Console.WriteLine(x == y);
+Console.WriteLine(x > y);
+Console.WriteLine(x < y);
+Console.WriteLine(x >= y);
+Console.WriteLine(x <= y);
+Console.WriteLine(x != y);
+
+Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+
+// Com String utilizando Equals.
+string a = "Cursos";
+string b = "cursos";
+
+Console.WriteLine($"Valor de a: {a}");
+Console.WriteLine($"Valor de b: {b}");
+
+Console.WriteLine(a == b);
+Console.WriteLine(a.Equals(b));
+
+Console.ReadKey();
