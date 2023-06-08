28201a1bad3e936f52a4f5f44b5384417bb1d15a
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-08 20:22:48 -0300

    Feature: Operador Uniário e Ternário

---
+++ 02_Fundamentos/CSFundamentos1/OperadorUniarioETernario/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Operador Uniário e Ternário\n");
+
+int positivo = 1;
+int resultado;
+
+resultado = +positivo;
+Console.WriteLine(positivo);
+
+Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+
+Console.WriteLine("Informe um número: \n");
+var n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"O negativo de {n} é {-n}");
+
+Console.WriteLine("Operador Ternário");
+
+Console.WriteLine("Informe a temperatura: \n");
+
+var temperatura = Convert.ToDouble(Console.ReadLine());
+
+var result = temperatura > 27 ? "Quente" : "Normal";
+Console.WriteLine($"O tempo está: {result}");
+
+
+Console.ReadKey();
