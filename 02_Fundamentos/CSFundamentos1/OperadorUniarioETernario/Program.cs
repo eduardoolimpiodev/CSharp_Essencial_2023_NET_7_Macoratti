cc034aa4191c67513ecf8547fc1c5fc74d9e19e4
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-08 20:27:23 -0300

    add: Operador condicional ternário.

--- 02_Fundamentos/CSFundamentos1/OperadorUniarioETernario/Program.cs
+++ 02_Fundamentos/CSFundamentos1/OperadorUniarioETernario/Program.cs
@@ -11,7 +11,9 @@ Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
 Console.WriteLine("Informe um número: \n");
 var n = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"O negativo de {n} é {-n}");
+Console.WriteLine($"O negativo de {n} é {-n}\n");
+
+Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
 
 Console.WriteLine("Operador Ternário");
 
@@ -22,5 +24,18 @@ var temperatura = Convert.ToDouble(Console.ReadLine());
 var result = temperatura > 27 ? "Quente" : "Normal";
 Console.WriteLine($"O tempo está: {result}");
 
+Console.WriteLine("Operador Condicional Ternário");
+
+Console.WriteLine("Informe o valor de X");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o valor de Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+string resultados = x > y ? "x é maior que y" :
+                                x < y ? "x é menor que y" :
+                                x == y ? "x é igual a y" : "Sem resultado";
+
+Console.WriteLine(resultados);
 
 Console.ReadKey();
