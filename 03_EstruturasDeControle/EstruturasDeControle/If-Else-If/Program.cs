6d3ec1ece3f1dc9911f2eb0abaa911713343247d
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-08 22:49:30 -0300

    Feature: Instrução If-Else-If

---
+++ 03_EstruturasDeControle/EstruturasDeControle/If-Else-If/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("If-Else-If\n");
+
+Console.WriteLine("Digite a nota do aluno: \t");
+var nota = Convert.ToDouble(Console.ReadLine());
+
+if (nota < 5)
+{
+    Console.WriteLine("Aluno Reprovado!");
+}
+else if(nota >= 5 && nota < 6)
+{
+    Console.WriteLine("Aluno em recuperação!");
+}
+else if(nota >= 6 && nota <= 9)
+{
+    Console.WriteLine("Aluno Aprovado!");
+}
+else
+{
+    Console.WriteLine("Aluno Aprovado com Distinção");
+}
+
+Console.WriteLine("FIM DO PROGRAMA!");
+
+Console.ReadKey();
