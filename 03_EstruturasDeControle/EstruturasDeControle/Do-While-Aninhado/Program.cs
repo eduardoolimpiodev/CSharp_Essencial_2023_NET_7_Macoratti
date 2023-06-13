85b7d9e6c2af07c766920cacea445cbbfa759cf3
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-13 01:58:37 -0300

    Feature: Do-While aninhada

---
+++ 03_EstruturasDeControle/EstruturasDeControle/Do-While-Aninhado/Program.cs
@@ -0,0 +1,24 @@
+﻿//Executa o código pelo menos uma vez, antes de avaliar a condição, depois executa o código novamente e avalia novamente a condição.
+Console.WriteLine("Loop Do-While Aninhado\n");
+
+
+int x = 0;
+do
+{
+    int y = 0;
+    do
+    {
+        Console.Write($"({x},{y})");
+        y++;
+    }
+    while (y < 5);
+    x++;
+    Console.WriteLine();
+}
+while (x < 5);
+
+
+Console.WriteLine("\nFim do programa...");
+
+
+Console.ReadLine();
