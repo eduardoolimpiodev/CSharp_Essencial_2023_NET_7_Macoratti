a71512f574cacc2ad8a4e1bf326f2235de027f89
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-12 23:38:28 -0300

    Feature: While com Break

---
+++ 03_EstruturasDeControle/EstruturasDeControle/While_02/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Instrução While 02\n");
+
+while(true)
+{
+    Console.WriteLine("\nInforme um número inteiro: (Para sair, tecle 999)");
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    if(numero == 999)
+    {
+        break;
+    }
+
+    if(numero%2 == 0)
+    {
+        Console.WriteLine($"{numero} é PAR!");
+    }
+    else
+        Console.WriteLine($"{numero} é ÍMPAR!");
+
+}
+
+Console.WriteLine("\nFim do processamento...");
+Console.ReadKey();  
