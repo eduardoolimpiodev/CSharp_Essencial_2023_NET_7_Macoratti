5b06210b2396bfffe8f02f474d092ba12f8f46c0
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-13 01:53:36 -0300

    Feature: Intrução do while

---
+++ 03_EstruturasDeControle/EstruturasDeControle/Do-While/Program.cs
@@ -0,0 +1,19 @@
+﻿//Executa o código pelo menos uma vez, antes de avaliar a condição, depois executa o código novamente e avalia novamente a condição.
+Console.WriteLine("Loop Do-While\n");
+
+
+var i = 1;
+
+do
+{
+    Console.WriteLine($"i = {i}");
+    i++;
+    if (i > 7)
+        break;
+}
+while (i <= 10);
+
+Console.WriteLine("\nFim do programa...");
+
+
+Console.ReadLine();
