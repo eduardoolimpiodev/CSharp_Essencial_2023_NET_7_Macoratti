8f68fa1deda952cc0a72890b82a712e4c7a530e8
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-12 22:52:13 -0300

    Feature: Estrutura de repetição com GOTO

---
+++ 03_EstruturasDeControle/EstruturasDeControle/EstruturaDeRepeticao/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Estruturas de repetição com goto \n");
+
+int i = 1;
+
+repetir:
+Console.WriteLine($"i= {i}");
+i++;
+
+if (i <= 10)
+    goto repetir;
+
+
+Console.WriteLine("Fim do processamento...");
+
+
+Console.ReadKey();
+
+
