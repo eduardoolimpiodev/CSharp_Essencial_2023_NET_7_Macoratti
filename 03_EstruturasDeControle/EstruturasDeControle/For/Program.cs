137c81d6f39e77316bb9a3ac48c5d48fed3abb3a
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-13 19:16:13 -0300

    Feature: Instrução FOR_01

---
+++ 03_EstruturasDeControle/EstruturasDeControle/For/Program.cs
@@ -0,0 +1,23 @@
+﻿// Executa o bloco de repetições repetidamente até que a condição específica retorne false.
+
+Console.WriteLine("Laço FOR\n");
+
+int resultado, numero;
+Console.WriteLine("Informe o número inteiro maior que zero: \t");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero > 0)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        resultado = numero * i;
+        Console.WriteLine($"{numero} X {i} = {resultado}");
+    }
+}
+else
+{
+    Console.WriteLine("Número inválido!");
+}
+Console.WriteLine("\nFim do processamento...");
+
+Console.ReadKey();
