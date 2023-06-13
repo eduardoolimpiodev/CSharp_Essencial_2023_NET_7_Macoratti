5f7b2a14f12327b0db7b1539b5e8708650b04bc6
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-12 23:44:19 -0300

    Feature: Instrução While Aninhada

---
+++ 03_EstruturasDeControle/EstruturasDeControle/While_03/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Instrução While Aninhado #03\n");
+Console.WriteLine("Coordenadas de um plano cartesiano para x e y\n");
+
+int x = 0;
+while(x < 5)
+{
+    int y = 0;
+    while (y < 5)
+    {
+        Console.Write($"({x},{y}) ");
+        y++;
+    }
+    x++;
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("\nFim do processamento...");
+Console.ReadKey();
