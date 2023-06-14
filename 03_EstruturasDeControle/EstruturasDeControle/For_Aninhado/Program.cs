4d44f3d3c72c5475076debeccdea215679f147bc
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-13 22:25:20 -0300

    Feature: Loop For Aninhado

---
+++ 03_EstruturasDeControle/EstruturasDeControle/For_Aninhado/Program.cs
@@ -0,0 +1,16 @@
+﻿// Executa o bloco de repetições repetidamente até que a condição específica retorne false.
+
+Console.WriteLine("Laço FOR Aninhado\n");
+
+for(int x = 0; x<5; x++)
+{
+    for(int y = 0; y<5; y++)
+    {
+        Console.Write($"({x}, {y}) ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nFim do processamento...");
+
+Console.ReadKey();
