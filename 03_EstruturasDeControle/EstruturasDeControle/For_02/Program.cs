374f14ece67bea61f6df5e8a03fce24f11047f07
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-13 22:21:17 -0300

    Feature: Loop For 02

---
+++ 03_EstruturasDeControle/EstruturasDeControle/For_02/Program.cs
@@ -0,0 +1,22 @@
+﻿// Executa o bloco de repetições repetidamente até que a condição específica retorne false.
+
+Console.WriteLine("Laço FOR #02\n");
+
+for(int i = 0, j = 0; i + j <= 20; i++, j++)
+{
+    Console.WriteLine($"i = {i} e j = {j}");
+}
+
+Console.WriteLine();
+
+int u = 1;
+for (; u <= 10; )
+{
+    Console.WriteLine($"u = {u}");
+    u++;
+}
+
+
+Console.WriteLine("\nFim do processamento...");
+
+Console.ReadKey();
