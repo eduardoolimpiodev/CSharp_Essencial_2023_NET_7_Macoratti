81253b70be741b2aafb2a171335bf8318c1ec3bb
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-14 22:21:55 -0300

    Feature: Break e Continue

---
+++ 03_EstruturasDeControle/EstruturasDeControle/Break_Continue/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Instrução BREAk e CONTINUE\n");
+
+for (; ; )
+{
+    Console.WriteLine("Tecle algo (X sai)");
+    var opcao = Console.ReadLine();
+
+    Console.WriteLine(opcao.ToUpper());
+
+    if (opcao == "X" || opcao == "x")
+    {
+        Console.WriteLine("Tchau...");
+        break;
+    }
+}
+
+for(int i=1; i <= 10; i++)
+{
+    if (i == 4)
+        continue;
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\nFim do processamento...");
+
+Console.ReadKey();
