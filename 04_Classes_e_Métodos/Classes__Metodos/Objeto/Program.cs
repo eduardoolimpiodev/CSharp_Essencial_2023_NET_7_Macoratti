833ba3c5ef32d5d997b8e3dcb92356ae997c754f
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-17 00:35:47 -0300

    Feature: O que é um objeto

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Objeto/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("O que é um objeto\n");
+
+//Um objeto é uma instância de uma classe
+
+
+
+Cachorro bilu = new Cachorro();
+bilu.nome = "Bilu";
+bilu.cor = "Marrom";
+bilu.sexo = "macho";
+
+Cachorro pipoca = new Cachorro();
+pipoca.nome = "Pipoca";
+pipoca.cor = "Branco com pintas";
+pipoca.sexo = "macho";
+
+Console.WriteLine($"{bilu.nome} {bilu.cor} {bilu.sexo}");
+Console.WriteLine($"{pipoca.nome} {pipoca.cor} {pipoca.sexo}");
+
+
+public class Cachorro
+{
+    public string? nome { get; set; }
+    public string? cor { get; set; }
+    public string? sexo { get; set; }
+
+    //public void Comer();
+    //public void Latir();
+}
