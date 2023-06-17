1b67401d87c0a9cff7b6d30beca9522c5f0dacf9
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-17 15:48:46 -0300

    Feature: Métodos

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Metodos/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Metodos\n");
+
+
+MinhaClasse minhaClasse = new MinhaClasse();
+minhaClasse.Saudacao();
+minhaClasse.ExibirDataAtual();
+
+
+Console.ReadKey();
+
+class MinhaClasse
+{
+    public void Saudacao()
+    {
+        Console.WriteLine("Bem vindo!");
+        ExibirDataAtual();
+    }
+
+    public void ExibirDataAtual()
+    {
+        Console.WriteLine(DateTime.Now);
+    }
+}
+
+
+
