797fc9aa8c6b5e67b21879d20d779017e0a506d9
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-17 00:27:03 -0300

    Feature: Alocação de memória

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Alocacao_de_Memoria/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Alocacao de Memoria\n");
+
+
+int x = 100;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Eduardo";
+p1.Idade = 37;
+p1.Sexo = "Masculino";
+
+Console.WriteLine($"{p1.Nome} {p1.Idade} {p1.Sexo}");
+
+Pessoa p2 = p1;
+
+
+//Pessoa p2 = new Pessoa();
+//p2.Nome = "Maria";
+//p2.Idade = 72;
+//p2.Sexo = "Feminino";
+
+Console.WriteLine($"{p2.Nome} {p2.Idade} {p2.Sexo}");
+
+Console.ReadKey();
+
+class Pessoa
+{
+    public string? Nome { get; set; }
+    public int Idade { get; set; }
+    public string? Sexo { get; set; }
+}
