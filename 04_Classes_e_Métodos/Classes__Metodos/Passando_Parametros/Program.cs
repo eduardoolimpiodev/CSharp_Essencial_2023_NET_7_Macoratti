2569fa24fcdfd8b7e37bc7840bb37bac95b1692a
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-17 20:23:22 -0300

    Feature: Passando parâmetros

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Passando_Parametros/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Passando Parametros\n");
+
+Aluno aluno = new Aluno();
+aluno.Nome = "Eduardo";
+aluno.Idade = 37;
+aluno.Sexo = "Masculino";
+aluno.Aprovado = "S";
+
+Console.WriteLine("Nome: ");
+aluno.Nome = Console.ReadLine();
+
+Console.WriteLine("Idade: ");
+aluno.Idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Sexo: ");
+aluno.Sexo = Console.ReadLine();
+
+Console.WriteLine("Aprovado: (S)im (N)ão");
+aluno.Aprovado = Console.ReadLine();
+
+Curso curso = new Curso();
+curso.Resultado(aluno);
+
+
+
+Console.ReadKey();
+
+public class Aluno
+{
+    public string? Nome { get; set; }
+    public int Idade { get; set; }
+    public string? Sexo { get; set; }
+    public string? Aprovado { get; set; }
+}
+
+
+
+public class Curso
+{
+    public void Resultado(Aluno aluno)
+    {
+        Console.WriteLine($"\nO Aluno: {aluno.Nome}, sexo: {aluno.Sexo} com {aluno.Idade} anos.");
+
+        if (aluno.Aprovado == "S")
+            Console.WriteLine("Foi aprovado");
+        else
+            Console.WriteLine("Foi reprovado");
+        
+    }
+}
