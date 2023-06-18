4935e4bffb94102a5ee1226e20fd0f229d38b090
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-17 23:42:50 -0300

    Feature: Metodo com Return

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Metodo_com_Return/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Metodo com Return\n");
+
+
+Calculadora calc = new Calculadora();
+int valor = calc.Somar(1, 2);
+Console.WriteLine(valor);
+Console.WriteLine(calc.Somar(1,2));
+
+
+Console.ReadKey();
+
+public class Calculadora
+{
+    public int Somar(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+
+    public int Subtrair(int n1, int n2)
+    {
+        return n1 - n2;
+    }
+
+    public int Multiplicar(int n1, int n2)
+    {
+        return n1 * n2;
+    }
+
+    public int Dividir(int n1, int n2)
+    {
+        return n1 / n2;
+    }
+}
