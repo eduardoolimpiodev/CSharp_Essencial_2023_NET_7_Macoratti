5738bc2802d02f16082c00651255a18dcf889453
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-18 00:49:41 -0300

    Feature: Exemplo prático de Métodos

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Metodos_Geral/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine("Metodos Geral\n");
+
+//Cliente cliente = new Cliente();
+
+Cadastro cadastro = new Cadastro();
+var cliente = cadastro.Registrar();
+
+cadastro.ExbirDados(cliente);
+
+//Alterar Renda
+cliente = cadastro.Registrar(cliente);
+cadastro.ExbirDados("Renda Alterada", cliente);
+
+Console.ReadKey();
+
+
+public class Cliente
+{
+    public string? Nome;
+    public int Idade;
+    public decimal Renda;
+
+    public Cliente(string? nome, int idade, decimal renda)
+    {
+        Nome = nome;
+        Idade = idade;
+        Renda = renda;
+    }
+
+    public Cliente()
+    {}
+}
+
+public class Cadastro
+{
+    public Cliente Registrar()
+    {
+        Cliente cliente = new("Eduardo", 37, 5000);
+        return cliente;
+    }
+
+    public Cliente Registrar(Cliente cliente)
+    {
+        cliente.Renda = 6000;
+        return cliente;
+    }
+
+    public void ExbirDados(Cliente cliente)
+    {
+        Console.WriteLine($"{cliente.Nome} {cliente.Idade} {cliente.Renda.ToString(("c"))}");
+    }
+
+    public void ExbirDados(string texto, Cliente cliente)
+    {
+        Console.Write($"\n{texto}\t");
+        Console.Write($"{cliente.Nome} {cliente.Renda.ToString(("c"))}");
+    }
+}
