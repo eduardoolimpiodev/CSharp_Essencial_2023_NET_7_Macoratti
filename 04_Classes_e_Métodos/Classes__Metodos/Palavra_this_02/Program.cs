84494ae8a9a5884230451f05e717012d9d2f46e7
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-17 22:43:53 -0300

    Feature: Palavra chave this 2

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Palavra_this_02/Program.cs
@@ -0,0 +1,20 @@
+﻿//Invocar outro construtor da mesma classe
+Console.WriteLine("Palavra this #02\n");
+
+Teste t1 = new Teste(999);
+
+
+Console.ReadKey();
+
+public class Teste
+{
+    public Teste(int num1, int num2)
+    {
+        Console.WriteLine($"\nConstrutor com dois parâmetros {num1} e {num2}");
+    }
+
+    public Teste(int num) : this(777, 888)
+    {
+        Console.WriteLine("\nContrutor com um parâmetro " + num);
+    }
+}
