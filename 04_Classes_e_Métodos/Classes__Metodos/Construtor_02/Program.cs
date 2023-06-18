9b67d3055f665933b61ffbcf630313753db25838
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-17 22:21:13 -0300

    Feature: Construtor 02

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Construtor_02/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Construtor #02\n");
+
+
+Aluno aluno = new Aluno("Eduardo",22, "Masculino", "S");
+
+
+Console.WriteLine(aluno.Nome == null ? "null" : aluno.Nome);
+Console.WriteLine(aluno.Idade == null ? "0" : aluno.Idade);
+Console.WriteLine(aluno.Sexo == null ? "null" : aluno.Sexo);
+Console.WriteLine(aluno.Aprovado == null ? "null" : aluno.Aprovado);
+
+Console.ReadKey();
+
+
+public class Aluno
+{
+    public Aluno(string nome) => Nome = nome;
+    
+
+    public Aluno(string nome, int idade, string sexo, string aprovado) : this(nome)
+    {
+        Idade = idade;
+        Sexo = sexo;
+        Aprovado = aprovado;
+    }
+
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+    public string? Aprovado;
+}
+
+
+
