6081ae8df83a905123a327d395965818097f7217
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-18 00:34:16 -0300

    Feature: Sobrecarga de métodos

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Sobrecarga_de_Metodos/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Assinatura de Métodos - Sobrecarga\n");
+
+Email email = new Email();
+email.Enviar("empresa.com.br");
+email.Enviar("empresa.com.br", "Proposta Comercial");
+email.Enviar("Empresa.com.br", 2000);
+email.Enviar(200, "empresa.com.br");
+
+
+
+Console.ReadKey();
+
+
+public class Email
+{
+    public void Enviar(string endereco)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine("Assunto padrão");
+    }
+
+    public void Enviar(string endereco, string assunto)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine($"{assunto}");
+    }
+
+    public void Enviar(string endereco, decimal valor)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine("Proposta Comercial");
+        Console.WriteLine($"{valor}");
+    }
+
+    public void Enviar(decimal valor, string endereco)
+    {
+        Console.WriteLine($"{endereco}");
+        Console.WriteLine("Pagamento de Fornecedor");
+        Console.WriteLine($"{valor}");
+    }
+}
