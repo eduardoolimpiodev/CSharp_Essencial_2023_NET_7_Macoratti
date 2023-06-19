bc7a5936dd87802ecad5c470b330234b113b3bb2
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-19 13:56:46 -0300

    Feature: Passagem de argumento por valor

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Passagem_de_Argumentos/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Passagem de argumentos por valor e referência\n");
+
+int x = 20;
+Console.WriteLine("\nValor do argumento x antes de passar por valor: " + x);
+
+Calculo calc = new Calculo();
+calc.Dobrar(x);
+
+Console.WriteLine("\nValor do argumento x depois de passar por valor: " + x);
+
+
+Console.ReadKey();
+
+public class Calculo
+{
+    public void Dobrar(int y)
+    {
+        y *= 2;
+        Console.WriteLine("\nValor do parâmetro y no método Dobrar " + y);
+    }
+}
