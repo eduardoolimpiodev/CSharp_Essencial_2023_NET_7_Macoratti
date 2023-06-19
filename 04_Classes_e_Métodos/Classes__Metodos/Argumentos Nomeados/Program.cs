91798fc740f2bcaf9a927a084b590010c12e0e57
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-19 18:18:33 -0300

    Feature: Argumentos nomeados

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Argumentos Nomeados/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Argumentos  nomeados\n");
+
+Email email = new Email();
+email.Enviar(destino:"Teste E-mail",titulo: "URGENTE",assunto: "Reunião de orçamentos");
+
+
+Console.ReadLine();
+
+public class Email
+{
+    public void Enviar(string destino, string titulo, string assunto)
+    {
+        Console.WriteLine($"\nPara {destino} - {titulo} \n Assunto: {assunto}");
+    }
+}
