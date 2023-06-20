ce4dd1f7133498feb778568a9fe0003fdfda30f8
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-20 18:59:49 -0300

    Feature: Parâmetros opcionais.

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Parametros_Opcionais/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Parâmetros opcionais\n");
+
+Console.WriteLine("Informe o destino: ");
+string destino = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Informe o Título: ");
+string titulo = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Informe o Assunto: ");
+string assunto = Convert.ToString(Console.ReadLine());
+
+Email email = new Email();
+email.Enviar();
+email.Enviar(destino);
+email.Enviar(destino, titulo);
+email.Enviar(destino, titulo, assunto);
+
+
+Console.ReadKey();
+
+public class Email
+{
+    public void Enviar(string destino = "Destino Padrão", string titulo = "Titulo Padrão", string assunto = "Assunto Padrão")
+    {
+        Console.WriteLine($"\nPara {destino} = {titulo} \n Assunto: {assunto}");
+    }
+}
+   
+
