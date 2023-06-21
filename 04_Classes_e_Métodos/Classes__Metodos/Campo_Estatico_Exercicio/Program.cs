ba4ea5305f3804c1e7cde12ffdd2f7517b24f771
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-21 19:31:28 -0300

    Feature: Exercício de campo estático

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Campo_Estatico_Exercicio/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Campo Estático - Exercício\n");
+
+ContaCorrente c1 = new ContaCorrente();
+c1.Conta = 101;
+c1.Nome = "Maria";
+c1.Juros = 3.99f;
+
+ContaCorrente c2 = new ContaCorrente();
+c2.Conta = 102;
+c2.Nome = "Eduardo";
+c2.Juros = 4.99f;
+
+Console.WriteLine();
+
+
+Console.ReadKey();
+
+public class ContaCorrente
+{
+    public int Conta;
+    public string? Nome;
+    public float Juros;
+
+    public float JurosAnual()
+    {
+        return Juros * 12;
+    }
+}
