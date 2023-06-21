b3e37a2639e7efdc9b415c19c514bb1d15a8e337
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-21 15:38:52 -0300

    Feature: Métodos estaticos

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Metodos_estaticos/Program.cs
@@ -0,0 +1,42 @@
+﻿// Os métodos estáticos são métodos que não dependem de um objeto, nao dependem da criação de uma instância de classe para ser acessado, ele não
+//pode utilizar variáveis do objeto. Ex.: A classe Math, console e método Main.
+Console.WriteLine("Metodo Estatico\n");
+
+
+
+
+var soma = Calculadora.Somar(10, 10);
+var subtrair = Calculadora.Subtrair(10, 10);
+var multiplicar = Calculadora.Multiplicar(10, 10);
+var dividir = Calculadora.Dividir(10, 10);
+
+Console.WriteLine(soma);
+Console.WriteLine(subtrair);
+Console.WriteLine(multiplicar);
+Console.WriteLine(dividir);
+
+Console.ReadLine();
+
+
+public class Calculadora
+{
+    public static int Somar(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+
+    public static int Subtrair(int n1, int n2)
+    {
+        return n1 - n2;
+    }
+
+    public static int Multiplicar(int n1, int n2)
+    {
+        return n1 * n2;
+    }
+
+    public static int Dividir(int n1, int n2)
+    {
+        return n1 / n2;
+    }
+}
