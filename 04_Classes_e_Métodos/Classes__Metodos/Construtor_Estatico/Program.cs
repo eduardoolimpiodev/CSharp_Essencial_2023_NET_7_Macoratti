64d8b4246b5c83e8cf4de50208a92d9c0e2250ad
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-23 17:47:48 -0300

    Feature: Construtor Estático.

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Construtor_Estatico/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Construtor Estático\n");
+
+Pessoa p1 = new("Maria", 18);
+
+Console.WriteLine(p1.Nome + " - " + p1.Idade);
+Console.WriteLine("Idade Mínima : " + Pessoa.IdadeMinima);
+
+
+
+Console.ReadKey();
+
+
+public class Pessoa
+{
+    public static int IdadeMinima;
+
+    public string? Nome { get; set; } = string.Empty;
+    public int Idade { get; set; }
+
+    public Pessoa(string? nome, int idade)
+    {
+        Console.WriteLine($"\nExecutando o contrutor parametrizado");
+        Nome = nome;
+        Idade = idade;
+    }
+
+    public Pessoa() { }
+
+    static Pessoa()
+    {
+        Console.WriteLine("\nExecutando o construtor estático.");
+        Console.WriteLine("Inicializando o campo IdadeMinima");
+        IdadeMinima = 18;
+    }
+}
