c2a6a0004cba34c9fd77ac901778faab9662152b
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-24 18:17:34 -0300

    Feature: Modificadores de Acesso

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Modificadores_de_Acesso/MinhaEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificadores_de_Acesso
+{
+    internal enum MinhaEnum
+    {
+        opcao1,
+        opcao2
+    }
+}
---
+++ 04_Classes_e_Métodos/Classes__Metodos/Modificadores_de_Acesso/Program.cs
@@ -0,0 +1,28 @@
+﻿using Modificadores_de_Acesso;
+
+Console.WriteLine("Modificadores de acesso\n");
+
+
+MinhaClasse minhaClasse = new MinhaClasse();
+
+MinhaEnum minhaEnum = new MinhaEnum();
+
+
+Console.ReadKey();
+
+
+
+internal class MinhaClasse
+{
+    int MeuCampo;
+
+    string? MinhaPropriedade { get; set; }
+
+    void MeuMetodo()
+    {
+        Console.WriteLine("Meu Método");
+    }
+
+}
+
+
