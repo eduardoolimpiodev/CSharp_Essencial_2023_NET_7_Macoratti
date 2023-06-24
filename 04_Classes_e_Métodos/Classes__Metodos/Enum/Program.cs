4d35f23e56a87c06987218c8f67f9fe5eb2820b3
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-24 14:00:50 -0300

    Feature: Enum

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Enum/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enum\n");
+
+int dia = (byte)DiasDaSemana.Quarta;
+Console.WriteLine(DiasDaSemana.Quarta);
+Console.WriteLine($"DiasDaSemana.Quarta vale: {dia}");
+
+Console.ReadKey();
+
+
+enum DiasDaSemana : byte
+{
+    Segunda, //0
+    Terca,   //1
+    Quarta,  //2
+    QUinta,  //3
+    Sexta,   //4
+    Sabado,  //5
+    Domingo  //6
+}
+
