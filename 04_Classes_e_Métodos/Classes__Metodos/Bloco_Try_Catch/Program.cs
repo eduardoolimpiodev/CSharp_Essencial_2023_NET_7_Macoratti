e549720bb3f216f546c81e441ba003adc3b3f6f5
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-25 22:27:16 -0300

    Feature: Bloco try catch

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Bloco_Try_Catch/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Bloco Try Catch - Tratamento de Erros\n");
+
+Console.WriteLine("Divisão de números inteiros");
+Console.WriteLine(" x / y");
+
+Console.WriteLine("\nInforme o valor de X");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nInforme o valor de Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+try
+{
+    int z = x / y;
+    Console.WriteLine($"\n{x} / {y} = {z}");
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine("\nNão existe divisão por zero, tente outro número.");
+
+    Console.WriteLine($"\nErro: <<< {ex.Message} >>>");
+    Console.WriteLine($"\nDetalhes : {ex?.StackTrace?.ToString()}");
+}
+finally
+{
+    Console.WriteLine("Fim do programa...");
+}
+
+
+Console.ReadKey();
