9ed0a331b9917f7e06cf7d84d2fe32159c2a3841
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-26 10:41:29 -0300

    Feature: partial Class

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Partial_Class/MinhaPartialClassProps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_Class
+{
+    public partial class MinhaPartialClass
+    {
+        public DateTime DataNascimento { get; set; }
+
+        public string? Nome { get; set; }
+    }
+}
