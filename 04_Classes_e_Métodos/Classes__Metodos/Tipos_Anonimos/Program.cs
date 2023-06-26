5a0536fbcccf7c66d1db1c1799d8cba872e623b3
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-25 22:52:07 -0300

    Feature: Tipos anônimos

---
+++ 04_Classes_e_Métodos/Classes__Metodos/Tipos_Anonimos/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Tipos Anônimos\n");
+
+var aluno = new
+{
+    Nome = "Eduardo",
+    Idade = 37,
+    Email = "contact@eduardoolimpio.com",
+    Endereco = new { Id = 1, Cidade = "Vix", Pais = "Brasil" }
+};
+
+Console.WriteLine($"\n{aluno.Nome}");
+Console.WriteLine($"\n{aluno.Endereco.Cidade}");
+
+
+Console.WriteLine(aluno.GetType().ToString());
+
+Console.WriteLine(aluno.Nome + " - " + aluno.Idade + " anos");
+
+Console.ReadKey();
+
+
+
