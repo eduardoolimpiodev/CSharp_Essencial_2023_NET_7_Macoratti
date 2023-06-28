287492e3c4b8b330101ed89620bab366958e3d70
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-27 21:53:54 -0300

    Array List...Continuar...

---
+++ 05_Array_ArrayList_List/Array_ArrayList_List/Arrays_02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Arrays #02\n");
+
+
+int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, };
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Elemento do índice {i} : {numeros[i]}");
+}
+
+Console.ReadKey();
