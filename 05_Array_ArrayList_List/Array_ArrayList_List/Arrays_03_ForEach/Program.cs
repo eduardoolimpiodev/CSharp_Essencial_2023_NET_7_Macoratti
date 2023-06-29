57c734cd8ef74ac8ae6c083e4c36d7143c2f2179
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-29 10:52:58 -0300

    Feature: Arrays #03 com Laço ForEach

---
+++ 05_Array_ArrayList_List/Array_ArrayList_List/Arrays_03_ForEach/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Arrays #03 - Instrução ForEach");
+
+
+int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
+
+Console.ReadKey();
