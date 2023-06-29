796e23202de1d737d42805bb5be949632b71416f
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-29 10:45:10 -0300

    Feature: Arrays com laço for

--- 05_Array_ArrayList_List/Array_ArrayList_List/Arrays_02/Program.cs
+++ 05_Array_ArrayList_List/Array_ArrayList_List/Arrays_02/Program.cs
@@ -1,11 +1,23 @@
 ﻿Console.WriteLine("Arrays #02\n");
 
 
-int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, };
+int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+string[] nomes = { "Eduardo", "Marcele", "Maria", "Paula", "Roberta", "Roberto"};
+
 
 for (int i = 0; i < numeros.Length; i++)
 {
     Console.WriteLine($"Elemento do índice {i} : {numeros[i]}");
+}
+
+
+Console.WriteLine($"\n");
+
+
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"Elemento do íncide {i} : {nomes[i]}");
 }
 
 Console.ReadKey();
