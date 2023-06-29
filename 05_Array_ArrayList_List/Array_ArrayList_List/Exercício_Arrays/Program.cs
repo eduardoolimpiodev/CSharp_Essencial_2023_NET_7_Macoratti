5b3c7660d2524dad0af8732c1b8d4f944da5606b
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-29 13:08:10 -0300

    Feature: Exercício de Array

---
+++ 05_Array_ArrayList_List/Array_ArrayList_List/Exercício_Arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Exercício Arrays\n");
+
+
+string[] nomes = new string[5];
+double[] notas = new double[5];
+
+Console.WriteLine("### Alunos ###\n");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Informe o nome do aluno: ");
+    string? nome = Console.ReadLine();
+    nomes[i] = nome;
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Informe a nota do aluno: ");
+    double nota = Convert.ToDouble(Console.ReadLine());
+    notas[i] = nota;
+}
+
+Console.WriteLine("\nAlunos: ");
+foreach (string nome in nomes)
+{
+    Console.Write($"{nome} ");
+}
+
+Console.WriteLine("\nNotas: ");
+var somaNotas = 0.0;
+//Count retorna o número de elementos do array.
+var totalNotas = notas.Count();
+foreach (double nota in notas)
+{
+    somaNotas = somaNotas + nota;
+    Console.Write($"{nota} ");
+}
+
+Console.WriteLine($"\nMédia Aritimética : \n {somaNotas/totalNotas} ");
+
+
+Console.ReadKey();
