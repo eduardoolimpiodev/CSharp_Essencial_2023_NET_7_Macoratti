bcd2bd7a856a3809f2762d366aa9eab703c59346
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-29 19:55:31 -0300

    Feature: Arrays Multidimensionais #01

---
+++ 05_Array_ArrayList_List/Array_ArrayList_List/Arrays_Multidimensionais_01/Program.cs
@@ -0,0 +1,26 @@
+﻿//Aloca memória, declara e inicializa o array.
+Console.WriteLine("Arrays Multidimensionais\n");
+
+int[,] a;
+
+a = new int[2, 2];
+
+a[0, 0] = 0;
+a[0, 1] = 1;
+a[1, 0] = 2;
+a[1, 1] = 3;
+
+Console.WriteLine(a[0, 0]);
+Console.WriteLine(a[0, 1]);
+Console.WriteLine(a[1, 0]);
+Console.WriteLine(a[1, 1]);
+
+int[,] b = new int[2, 2] { { 20, 30 }, { 40, 50 } };
+
+Console.WriteLine(b[0, 0]);
+Console.WriteLine(b[0, 1]);
+Console.WriteLine(b[1, 0]);
+Console.WriteLine(b[1, 1]);
+
+
+Console.ReadKey();
