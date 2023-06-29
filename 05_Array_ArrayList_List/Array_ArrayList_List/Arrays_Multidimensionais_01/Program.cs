7abdc46ee11e7341adf56dbd2fea4cad224c6d6f
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-29 19:56:52 -0300

    add: WriteLine

--- 05_Array_ArrayList_List/Array_ArrayList_List/Arrays_Multidimensionais_01/Program.cs
+++ 05_Array_ArrayList_List/Array_ArrayList_List/Arrays_Multidimensionais_01/Program.cs
@@ -10,12 +10,17 @@ a[0, 1] = 1;
 a[1, 0] = 2;
 a[1, 1] = 3;
 
+Console.WriteLine("\nArray a\n");
+
+
 Console.WriteLine(a[0, 0]);
 Console.WriteLine(a[0, 1]);
 Console.WriteLine(a[1, 0]);
 Console.WriteLine(a[1, 1]);
 
 int[,] b = new int[2, 2] { { 20, 30 }, { 40, 50 } };
+
+Console.WriteLine("\nArray b\n");
 
 Console.WriteLine(b[0, 0]);
 Console.WriteLine(b[0, 1]);
