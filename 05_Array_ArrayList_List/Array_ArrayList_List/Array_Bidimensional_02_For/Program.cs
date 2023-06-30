a8e7ec5c5ea9253fda1bb50e1423762bafb84cc3
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-29 22:27:27 -0300

    Feature: Arrays Multidimensionais com laço For e ForEach

---
+++ 05_Array_ArrayList_List/Array_ArrayList_List/Array_Bidimensional_02_For/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Array Multidimensional - Laço For e ForEach\n");
+
+
+int[,] n = {
+             { 11, 22, 33 }, //n[0, 0]  n[0 ,1] n[0, 2]
+             { 44, 55, 66 }, //n[1, 0]  n[1 ,1] n[1, 2]  n[3,3] -> n[i, j]
+             { 77, 88, 99 }  //n[2, 0]  n[2 ,1] n[2, 2]
+            };
+
+Console.WriteLine("\nFOR\n");
+
+for (int i = 0; i < n.GetLength(0); i++)
+{
+    for (int j = 0; j < n.GetLength(1); j++)
+    {
+        Console.Write($"{n[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nFOR-EACH\n");
+
+foreach (var x in n)
+{
+    Console.Write($"{x} ");
+}
+
+Console.ReadKey();
