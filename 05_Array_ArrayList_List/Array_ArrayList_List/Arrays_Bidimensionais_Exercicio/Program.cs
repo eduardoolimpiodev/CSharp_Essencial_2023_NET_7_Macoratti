bf64e7cd2e9fd5b6e1704383e57d0161a8b48020
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-29 22:51:53 -0300

    Feature: Exercício de Arrays Bidimensionais.

---
+++ 05_Array_ArrayList_List/Array_ArrayList_List/Arrays_Bidimensionais_Exercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Exercício - Arrays Bidimensionais\n");
+
+//Duas linhas e Cinco colunas, declarei e aloquei memória para o array.
+string[,] alunos = new string[2, 5];  //alunos[i,j]
+
+Console.WriteLine("\nAtribuindo dados a um array de " + "strings com 2 linhas e 5 colunas [2, 5]\n");
+
+Console.WriteLine("{Maria, Paulo, José, Pedro, Carlos}");
+Console.WriteLine("{Silva, Pedro, Alicia, Manoel, Paula}\n");
+
+for (int i = 0; i < alunos.GetLength(0); i++)
+{
+	for (int j = 0; j < alunos.GetLength(1); j++)
+	{
+        Console.WriteLine($"Informe o valor para o elemento na posição [{i}, {j}] ");
+        alunos[i, j] = Console.ReadLine();
+    }
+}
+
+Console.WriteLine("\nConteúdo do Array alunos \n");
+
+for (int i = 0; i < alunos.GetLength(0); i++)
+{
+    for (int j = 0; j < alunos.GetLength(1); j++)
+    {
+        Console.Write($"[{i}, {j}] = {alunos[i, j]}\t ");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
