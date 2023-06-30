64977842009a0ecd546a16c4374046a5fa1e6863
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-06-30 11:37:41 -0300

    Feature: ArrayList #01

---
+++ 05_Array_ArrayList_List/Array_ArrayList_List/ArrayList_01/Program.cs
@@ -0,0 +1,26 @@
+﻿//Array List - Coleção não genérica cujo o tamanho pode ser alterado dinamicamente e armazena elementos de vários dados.
+
+using System.Collections;
+
+Console.WriteLine("Array List #01\n");
+
+ArrayList lista = new();
+lista.Add("Eduardo");
+lista.Add("Ramos");
+lista.Add("Junqueira");
+lista.Add("Olimpio");
+lista.Add(37);
+lista.Add("");
+lista.Add(true);
+lista.Add(null);
+
+
+//Inicializadores de objeto -> fazer a declaração, criar a instância e já inicializar sem a necessidade de criar um construtor.
+
+var lista2 = new ArrayList()
+{
+    "Eduardo", 37, true, " ", null
+};
+
+
+Console.ReadKey();
--- 05_Array_ArrayList_List/Array_ArrayList_List/ArrayList_01/obj/Release/net6.0/ArrayList_01.AssemblyInfo.cs
+++ 05_Array_ArrayList_List/Array_ArrayList_List/ArrayList_01/obj/Release/net6.0/ArrayList_01.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Arrays_Multidimensionais_02_For_e_FOrEach")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ArrayList_01")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Arrays_Multidimensionais_02_For_e_FOrEach")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Arrays_Multidimensionais_02_For_e_FOrEach")]
+[assembly: System.Reflection.AssemblyProductAttribute("ArrayList_01")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ArrayList_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
