99211e4ef1521ebec7807f2500eeb4c5fb26376c
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-01 18:12:08 -0300

    Feature: ArrayList #03 - Removendo Elementos

--- 05_Array_ArrayList_List/Array_ArrayList_List/Array_Bidimensional_02_For/obj/Debug/net6.0/Arrays_Multidimensionais_02_For_e_FOrEach.AssemblyInfo.cs
+++ 05_Array_ArrayList_List/Array_ArrayList_List/Array_Bidimensional_02_For/obj/Debug/net6.0/Arrays_Multidimensionais_02_For_e_FOrEach.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Arrays_Multidimensionais_02_For_e_FOrEach")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Arrays_Multidimensionais_02_For_e_ForEach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Arrays_Multidimensionais_02_For_e_FOrEach")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Arrays_Multidimensionais_02_For_e_FOrEach")]
+[assembly: System.Reflection.AssemblyProductAttribute("Arrays_Multidimensionais_02_For_e_ForEach")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Arrays_Multidimensionais_02_For_e_ForEach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
---
+++ 05_Array_ArrayList_List/Array_ArrayList_List/Array_List_03_Remover_Elementos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+Console.WriteLine("ArrayList #03 - Removendo Elementos\n");
+
+
+/* Métodos:
+ * Remove(Object value) - Remove a primeira ocorrência de um objeto específico.
+ * RemoveAt(int index) - Remove o elemento no índice especificado.
+ * RemoveRange(int index, int count) - Remove um número definido de elementos a partir de um índice especificado.
+ */
+
+
+var lista = new ArrayList()
+{
+    "Ana", 5, true, " ", null, 1.1, "Zé", 3, 9, 0
+};
+
+lista.Remove(null);
+
+lista.RemoveAt(4);
+
+lista.RemoveRange(0, 2);
+
+Console.ReadKey();
