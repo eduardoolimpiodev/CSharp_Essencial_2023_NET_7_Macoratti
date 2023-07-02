70b271207e860a424b002f2ba2fb795aa147bb50
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-02 17:34:30 -0300

    Feature: DIferença entre ArrayList e List<T>

---
+++ 05_Array_ArrayList_List/Array_ArrayList_List/List_e_ArrayList/Program.cs
@@ -0,0 +1,21 @@
+﻿//Usando o List<T> ganhamos segurança e performance, pois não há processo de boxing e unboxing e é fortemente tipado.
+
+using System.Collections;
+
+Console.WriteLine("List e ArrayList\n");
+
+ArrayList lista = new ArrayList();
+lista.Add(3);
+lista.Add(4);
+
+var elemento = lista[0];
+int numero = (int)lista[1];
+Console.WriteLine(numero);
+
+List<int> lista1 = new List<int>();
+lista1.Add(1);
+
+var item = lista[0];
+
+
+Console.ReadKey();
