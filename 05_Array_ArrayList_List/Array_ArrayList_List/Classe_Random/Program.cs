194bb1307a70b9e2052127806a609b5fc961b90c
eduardo.olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-05 11:35:32 -0300

    Feature: Classe Random

---
+++ 05_Array_ArrayList_List/Array_ArrayList_List/Classe_Random/Program.cs
@@ -0,0 +1,24 @@
+﻿//Usada para gerar números aleatórios com base no valor de uma semente ( seed );
+
+Console.WriteLine("Classe Random\n");
+
+Random random = new Random();
+
+Console.WriteLine(random.Next());
+Console.WriteLine(random.Next(20));
+Console.WriteLine(random.Next(0,20));
+Console.WriteLine(random.NextDouble());
+
+byte[] randBytes = new byte[10];
+Random random2 = new Random();
+random2.NextBytes(randBytes);
+
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(randBytes[i]);
+}
+
+
+
+Console.ReadKey();
