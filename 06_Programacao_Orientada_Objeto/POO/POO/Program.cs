07c722b0f36d17503e4e81e42f90055e15f67aea
eduardo.olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-05 14:28:29 -0300

    Feature: POO

--- 06_Programacao_Orientada_Objeto/POO/POO/Program.cs
+++ 06_Programacao_Orientada_Objeto/POO/POO/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*
- 
- 
+ POO - Um objeto é uma instância de uma classe.
+ A classe é o tipo, e o Objeto é a instância do tipo.
  
 */
 
@@ -10,3 +10,4 @@ Console.WriteLine("POO - Programação Orientada a Objeto\n");
 
 
 Console.ReadKey();
+ 
