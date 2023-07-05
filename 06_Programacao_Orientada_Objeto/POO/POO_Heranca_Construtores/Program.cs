4f487d0856f5450adf338bfaf209e391e347a17a
eduardo.olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-05 15:05:25 -0300

    Feature: Herança - Construtores

---
+++ 06_Programacao_Orientada_Objeto/POO/POO_Heranca_Construtores/Program.cs
@@ -0,0 +1,36 @@
+﻿//Tanto a classe base e a classe derivada podem possuir seus próprios construtores.
+//A classe derivada não vai herdar o construtor da classe base, mas pode invoca-lo.
+Console.WriteLine("### Herança - Construtores\n");
+
+
+Aluno aluno1 = new();
+Aluno aluno2 = new("Eduardo");
+
+
+Console.ReadKey();  
+
+//classe base
+class Pessoa
+{
+	public Pessoa()
+	{
+        Console.WriteLine("Construtor da classe Pessoa - Classe Base");
+    }
+    public Pessoa(string nome)
+    {
+        Console.WriteLine("Construtor da classe Pessoa - Classe Base - Com parâmetro.");
+    }
+}
+
+//classe derivada
+class Aluno : Pessoa
+{
+    public Aluno() : base()
+    {
+        Console.WriteLine("Construtor da classe Aluno - Classe Derivada");
+    }
+    public Aluno(string nome) : base(nome)
+    {
+        Console.WriteLine("Construtor da classe Aluno - Classe Derivada - Com parâmetro.");
+    }
+}
