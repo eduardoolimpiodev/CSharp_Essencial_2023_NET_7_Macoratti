709f7743460b8e683f6a32f856c7ec2dd351eec3
eduardo.olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-05 15:19:00 -0300

    Feature: Herança e Object

---
+++ 06_Programacao_Orientada_Objeto/POO/POO_Heranca_e_Object/Program.cs
@@ -0,0 +1,24 @@
+﻿//Toda classe é derivada da classe Object e a classe Object não deriva de nenhuma classe.
+//Ela é a classe Base (Pai) de todas as classes.
+//Uma classe somente pode herdar de outra classe e apenas de uma classe.
+/*
+ Principais métodos definidos na classe Object:
+    * Equals : Suporta comparações entre objetos.
+    * GetHashCode : Gera um numero hash para o objeto.
+    * ToString : Retorna a representação do objeto como uma string.
+    * GetType : Retorna informação sobre o tipo.
+*/
+Console.WriteLine("Herança e Object\n");
+
+Nada nada = new Nada();
+
+
+
+Console.ReadKey();
+
+
+public class Nada
+{
+
+}
+
