310cef72d0b0f51f9c1d61a11649a98c795cc0c8
eduardo.olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-06 09:27:55 -0300

    Feature: Modificador Sealed - Selar o método

--- 06_Programacao_Orientada_Objeto/POO/POO_Modificador_Sealed/Program.cs
+++ 06_Programacao_Orientada_Objeto/POO/POO_Modificador_Sealed/Program.cs
@@ -1,6 +1,38 @@
-﻿Console.WriteLine("Modificador Sealed\n");
+﻿//Quando aplicado a uma classe, o modificador sealed impede que outras classes herdem dela. Ou seja, impede a herança da classe base.
 
+Console.WriteLine("Modificador Sealed\n");
 
+Cliente cli = new();
+cli.Nome = "Eduardo";
+cli.ExibeNome();
 
 
 Console.ReadKey();
+
+
+//sealed class MinhaClasseBase
+//{
+
+//}
+
+//class MinhaSubclasse : MinhaClasseBase
+//{
+
+//}
+
+sealed class Pessoa
+{
+    public string? Nome { get; set; }
+    public void ExibeNome()
+    {
+        Console.WriteLine($"Meu nome é {Nome}");
+    }
+}
+
+class Cliente : Pessoa
+{
+    public void ExibeNome()
+    {
+        Console.WriteLine($"\nNome do cliente : {Nome}");
+    }
+}
