aeda9537aff5434334b146a076d69e91cfd36020
eduardo.olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-06 11:54:12 -0300

    Feature: Operador Is e As

---
+++ 06_Programacao_Orientada_Objeto/POO/POO_Operadores_Is_e_As/Program.cs
@@ -0,0 +1,9 @@
+﻿//Operador As : é usado para realizar a conversão entre tipos de referência ou tipos anuláveis compatíveis,
+//retorna null quando a conversão não for possível, não gera uma exceção.
+Console.WriteLine("Operadores Is e As\n");
+
+
+
+
+
+Console.ReadKey();
