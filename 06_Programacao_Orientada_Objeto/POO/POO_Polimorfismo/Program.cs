4f897e2e101667c304efe1ed328184d7e389fd9c
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-07 13:22:53 -0300

    Feature: Polimorfismo

---
+++ 06_Programacao_Orientada_Objeto/POO/POO_Polimorfismo/Program.cs
@@ -0,0 +1,9 @@
+﻿//Considerado o 3º pilar da POO. Depois de Encapsulamento e Herança.
+//1º Polimorfismo em tempo de compilação (Overloading ou Sobrecarga)
+//2º Polimorfismo em tempo de execução (Overriding ou Sobrescrita) - Late Binding/Ligação tardia
+
+Console.WriteLine("POO - Polimorfismo\n");
+
+
+
+Console.ReadKey();
