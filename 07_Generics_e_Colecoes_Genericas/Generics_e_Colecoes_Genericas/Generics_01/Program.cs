8744a6dd697faf64d1e28c7dad5ff0fa0cd9b0ed
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-07 14:56:21 -0300

    Feature: Generics #01 - Definição

---
+++ 07_Generics_e_Colecoes_Genericas/Generics_e_Colecoes_Genericas/Generics_01/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Generics: Forma geral e não a forma específica.
+Na linguagem C#, Generics significa não ser específico para um determinado tipo de dados.
+A linguagem C# permite definir classes, interfaces, campos, métodos, propiedades, eventos, delegates e operadores genéricos
+usando o parâmetro de tipo e sem o tipo de dados específico.
+O recurso Generics permite que classes, métodos e interfaces possam ser parametrizados por tipo.
+Já utilizamos o recurso Generics quando tratamos das coleções List<int>, List<string> e List<object>.
+*/
+
+Console.WriteLine("Generics\n");
+
+
+
+
+Console.ReadKey();
+
+public class ClasseGeneric<T>
+{ }
+
+public interface InterfaceGeneric<T>
+{ }
+
+public class ClasseGeneric<T1,T2>
+{ }
