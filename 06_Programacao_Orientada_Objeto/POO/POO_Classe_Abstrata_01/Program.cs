c5eaeadd5fcea3635447b28d49c5accf58e97565
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-07 09:42:40 -0300

    Feature: Classe Abstrata

---
+++ 06_Programacao_Orientada_Objeto/POO/POO_Classe_Abstrata_01/Program.cs
@@ -0,0 +1,27 @@
+﻿//O modificador abstratic indica que o recurso que está sendo modificado tem uma implementação ausente ou incompleta.
+
+using POO_Classe_Abstrata_01;
+
+Console.WriteLine("Classe Abstrata\n");
+
+Quadrado q = new Quadrado();
+Console.WriteLine(q.Descricao());
+
+Console.WriteLine("\nInforme a cor da figura");
+q.Cor =Console.ReadLine();
+
+Console.WriteLine("\nInforme o valor do lado do quadrado");
+q.Lado = Convert.ToInt32(Console.ReadLine());
+
+q.CalcularArea();
+q.CalcularPerimetro();
+
+Console.WriteLine($"\nÁrea do quadrado : {q.Area} m2");
+Console.WriteLine($"\nPerímetro do quadrado : {q.Perimetro} m");
+
+Console.WriteLine($"\nO quadrado tem a cor : {q.Cor}");
+
+
+
+Console.ReadKey();
+
---
+++ 06_Programacao_Orientada_Objeto/POO/POO_Classe_Abstrata_01/Quadrado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Classe_Abstrata_01
+{
+    public class Quadrado : Forma
+    {
+        public double Lado { get; set;}
+
+        public override void CalcularArea()
+        {
+            this.Area = Lado * Lado;
+        }
+
+        public override void CalcularPerimetro()
+        {
+            this.Perimetro = 4 * Lado;
+        }
+    }
+}
