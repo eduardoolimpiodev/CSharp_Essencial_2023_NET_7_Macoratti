e117e3911370a23bb2ea6fecf1b39bc5929570ba
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-07 13:37:47 -0300

    Polimorfismo em tempo de Execução, com sobrecargar de métodos.

---
+++ 06_Programacao_Orientada_Objeto/POO/POO_Polimorfismo_Tempo_de_Execucao/Program.cs
@@ -0,0 +1,25 @@
+﻿//Considerado o 3º pilar da POO. Depois de Encapsulamento e Herança.
+//1º Polimorfismo em tempo de compilação (Overloading ou Sobrecarga)
+//2º Polimorfismo em tempo de execução (Overriding ou Sobrescrita) - Late Binding/Ligação tardia
+
+Calcular calc = new Calcular();
+Console.WriteLine(calc.Somar(30,40));
+Console.WriteLine(calc.Somar(30,40,50));
+
+
+Console.ReadKey();
+
+
+
+class Calcular
+{
+    public int Somar(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public int Somar(int num1, int num2, int num3)
+    {
+        return num1 + num2 + num3;
+    }
+}
