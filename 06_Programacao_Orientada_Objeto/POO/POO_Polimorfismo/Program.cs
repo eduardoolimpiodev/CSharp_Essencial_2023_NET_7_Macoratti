51ffc2a270aa9884d69c6c30e74e5835f591413d
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-07 13:34:46 -0300

    Feature: Polimorfismo em tempo de compilação - Overloading

--- 06_Programacao_Orientada_Objeto/POO/POO_Polimorfismo/Program.cs
+++ 06_Programacao_Orientada_Objeto/POO/POO_Polimorfismo/Program.cs
@@ -2,7 +2,21 @@
 //1º Polimorfismo em tempo de compilação (Overloading ou Sobrecarga)
 //2º Polimorfismo em tempo de execução (Overriding ou Sobrescrita) - Late Binding/Ligação tardia
 
+using POO_Polimorfismo;
+
 Console.WriteLine("POO - Polimorfismo\n");
+
+var figuras = new List<Figura>
+{
+    new Triangulo(),
+    new Circulo()
+};
+
+foreach (var figura in figuras)
+{
+    figura.Desenhar();
+}
+
 
 
 
---
+++ 06_Programacao_Orientada_Objeto/POO/POO_Polimorfismo/Triangulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Polimorfismo
+{
+    public class Triangulo : Figura
+    {
+        public override void Desenhar()
+        {
+            Console.WriteLine("Desenhando um triângulo...");
+            base.Desenhar();
+        }
+    }
+}
