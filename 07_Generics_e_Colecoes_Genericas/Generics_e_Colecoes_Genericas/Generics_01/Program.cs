8ed80a4f94d79212ee09476e36b45a3e5ba031bb
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-07 15:03:05 -0300

    Add: Benefícios de utilizar Generics

--- 07_Generics_e_Colecoes_Genericas/Generics_e_Colecoes_Genericas/Generics_01/Program.cs
+++ 07_Generics_e_Colecoes_Genericas/Generics_e_Colecoes_Genericas/Generics_01/Program.cs
@@ -5,6 +5,17 @@ A linguagem C# permite definir classes, interfaces, campos, métodos, propiedade
 usando o parâmetro de tipo e sem o tipo de dados específico.
 O recurso Generics permite que classes, métodos e interfaces possam ser parametrizados por tipo.
 Já utilizamos o recurso Generics quando tratamos das coleções List<int>, List<string> e List<object>.
+Benefícios de utilizar Generics: 
+    1-Reutilização de código.
+    2-Segurança de tipo (Type Safety)
+    3-Desempenho (evita as operações e boxing e unboxing)
+
+Generics é usado para definir as coleções genéricas no namespace System.Collection.Generic
+    Collection<T>
+    List<T>
+    Dictionary<Tkey, Tvalue>
+    Queue<T>
+    Stack<T>
 */
 
 Console.WriteLine("Generics\n");
@@ -22,3 +33,8 @@ public interface InterfaceGeneric<T>
 
 public class ClasseGeneric<T1,T2>
 { }
+
+public class ClasseGeneric1<T1, T2> where T1: struct where T2 : class { }
+
+public class ClasseGeneric2<T> where T : class { }
+
