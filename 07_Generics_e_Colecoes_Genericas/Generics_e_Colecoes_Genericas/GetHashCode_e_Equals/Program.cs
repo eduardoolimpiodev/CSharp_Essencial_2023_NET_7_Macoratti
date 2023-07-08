70f3b6b323a28fd2d64f71493e7211f61a703561
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-07 21:27:57 -0300

    Feature: GetHashCode e Equals

---
+++ 07_Generics_e_Colecoes_Genericas/Generics_e_Colecoes_Genericas/GetHashCode_e_Equals/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+    GetHashCOde e Equals são métodos da classe Object que são usados para comparar se dois objetos são iguais.
+    Todos os objetos no C# herdam de Object e assim herdam os métodos GetHashCode e Equals.
+*/
+
+Console.WriteLine("GetHashCode e Equals\n");
+
+
+
+Console.ReadKey();
