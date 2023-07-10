78f217514f5c5417ee1b73f3977adfe54bac1bda
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-10 13:14:54 -0300

    Feature: Coleções Set

---
+++ 07_Generics_e_Colecoes_Genericas/Generics_e_Colecoes_Genericas/Colecao_Set/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ - HashSet<T> e SortedSet<T>
+    * Uma coleção que contém apenas items distintos é conhecida pelo conjunto de termos ou Set.
+    * São dois conjuntos ou sets definidos como coleções genéricas usadas para armazenar e manipular conjuntos de dados não dupli
+      cados e estão presentes no namespace System.Collection.Generics.
+    * Em termos de desempenho, HashSet<T> geralmente é mais rápido que SortedSet<T> para operações como adicionar e remover elementos,
+      mas SortedSet<T> é mais rápido para operações que dependem da ordem dos elementos, como localizar o primeiro ou o último elemento.
+ - HashSet<T> é uma coleção genérica não ordenada que implementa a interface ISet<T> e representa um conjunto de itens exclusivos
+   (não permite a inclusão de elementos duplicados).
+   Usa uma função Hash para determinar a posição de um item na coleção, o que permite o acesso rápido de elementos.
+   A interface ISet<T> oferece métodos para criar uma união de múltiplos conjuntos, para criar uma interseção de conjuntos, ou para
+   fornecer informações se um conjunto for um superconjunto ou subconjunto de outro.
+*/
+Console.WriteLine("Coleção Set - HashSet<T> e SortedSet<T> \n");
+
+
+
+
+
+Console.ReadKey();
