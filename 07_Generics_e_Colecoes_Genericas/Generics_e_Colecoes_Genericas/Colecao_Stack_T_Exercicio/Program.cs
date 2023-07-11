6d606a48f76bae851ceb36a3963146301863f148
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-07-11 10:59:35 -0300

    Feature: Stack<T> - Undo

---
+++ 07_Generics_e_Colecoes_Genericas/Generics_e_Colecoes_Genericas/Colecao_Stack_T_Exercicio/Program.cs
@@ -0,0 +1,27 @@
+﻿//Undo - desfazer
+using Colecao_Stack_T_Exercicio;
+
+Console.WriteLine("Coleção Stack<T> - Undo - Exercício\n");
+
+EditorTexto editor = new EditorTexto();
+Console.WriteLine("\nDigitando...\n");
+editor.DigitarChar('S');
+editor.DigitarChar('t');
+editor.DigitarChar('a');
+editor.DigitarChar('c');
+editor.DigitarChar('q');
+
+Console.WriteLine("\nFazendo o Undo\n");
+editor.Undo();
+editor.Undo();
+
+Console.WriteLine("\nRedigitando...\n");
+editor.DigitarChar('c');
+editor.DigitarChar('k');
+editor.DigitarChar('!');
+
+Console.ReadLine();
+
+
+
+
