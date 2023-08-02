f0c7dcd94434e7dd492c0e7d1079d4dab6026e73
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-08-02 13:36:48 -0300

    Add: Read_Only_Collection_T_01.

---
+++ 07_Generics_e_Colecoes_Genericas/Generics_e_Colecoes_Genericas/Read_Only_Collection_T/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+Console.WriteLine("ReadOnly Collection\n");
+
+/*
+    * São estruturas de dados que permitem que os dados sejam acesados, mas não modificados.
+    
+    * ReadOnlyCollection<T>:
+     - Fornece a classe base para uma coleção somente leitura genérica.
+     - É criada a partir de uma coleção mutável (Array ,List).
+    * ReadOnlyDisctionary<TKey, TValue>:
+     - Representa uma coleção somente leitura genérica de pares chave/valor.
+     - Ela é criada a partir de um dicionário mutável.
+ */
+
+
+var planetas = new List<string>() { "Mercúrio", "Vênus", "Terra", "Marte"};
+var listaPLanetas = new ReadOnlyCollection<string>(planetas);
+Console.WriteLine(listaPLanetas);
+
+
+Console.ReadKey();
