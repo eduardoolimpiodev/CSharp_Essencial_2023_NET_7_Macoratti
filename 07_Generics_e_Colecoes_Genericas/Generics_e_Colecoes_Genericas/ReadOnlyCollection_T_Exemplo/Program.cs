2d11bd56edd2e2fa153c15336b4959b0d9c3b6af
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-08-03 11:35:37 -0300

    Add: ReadOnlyCollection<T> Exemplo.

---
+++ 07_Generics_e_Colecoes_Genericas/Generics_e_Colecoes_Genericas/ReadOnlyCollection_T_Exemplo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+
+Console.WriteLine("ReadOnly Collection - Exemplo\n");
+
+var exoPlanetas = new ReadOnlyCollection<ExoPlaneta>(ExoPlaneta.GetExoPlanetas());
+
+foreach (var item in exoPlanetas)
+{
+    Console.WriteLine(item.Nome);
+}
+
+Console.ReadKey();
+
+class ExoPlaneta
+{
+    public string? Nome { get; set; }
+
+    public static List<ExoPlaneta> GetExoPlanetas()
+    {
+        return new List<ExoPlaneta>()
+        {
+            new ExoPlaneta() { Nome = "Proxima Centauri b"},
+            new ExoPlaneta() { Nome = "Kleper 186-f"},
+            new ExoPlaneta() { Nome = "Gliesi 1061-c"},
+        };
+    }
+}
