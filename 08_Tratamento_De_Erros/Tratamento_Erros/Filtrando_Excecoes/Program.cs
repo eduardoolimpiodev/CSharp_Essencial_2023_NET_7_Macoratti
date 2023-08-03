c8d5af5f332fc3b5419172ca998650451c8edfeb
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-08-03 16:02:44 -0300

    Filtros de Exceções.

---
+++ 08_Tratamento_De_Erros/Tratamento_Erros/Filtrando_Excecoes/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Filtrando Exceções\n");
+
+
+try
+{
+    Console.WriteLine("\nInforme o dividendo");
+    int dividendo = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("\nInforme o divisor");
+    int divisor = Convert.ToInt32(Console.ReadLine());
+
+    int resultado = (dividendo / divisor);
+    Console.WriteLine($"\n{dividendo} / {divisor} = {resultado}\n");
+
+    Console.ReadKey();
+}
+catch (DivideByZeroException ex) when (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+{
+    Console.WriteLine($"\n Hoje é sexta-feira e não capturamos estas exeções por motivos óbvios...\n\nSextouuuuuu\n\n");
+    //Console.WriteLine($"\n{ex.GetType()} informa : {ex.Message}");
+    //Console.WriteLine($"\n{ex.Message}");
+    //Console.WriteLine($"\n{ex.Source}");
+    //Console.WriteLine($"\n{ex.StackTrace}");
+}
+catch (Exception ex) when (ex.Message.Contains("format"))
+{
+    Console.WriteLine("filtro de exceção com format.");
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.StackTrace);
+    Console.WriteLine(ex.Message);
+}
+
+
+
+
+
+Console.ReadKey();
