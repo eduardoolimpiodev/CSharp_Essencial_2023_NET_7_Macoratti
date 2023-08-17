6fc2a45fcfb9df19f8171ea04aed5f06b093af3a
Eduardo Olimpio <eduardo.olimpio.developer@gmail.com>
2023-08-17 12:52:39 -0300

    Expressão Lambda

---
+++ 09_Delegates_Lambda_Eventos_Linq/Secao_09/Expressao_Lambda/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Expressão Lambda\n");
+
+
+List<string> nomes = new List<string>();
+nomes.Add("Eduardo");
+nomes.Add("Maria");
+nomes.Add("Marcele");
+nomes.Add("Roberto");
+
+//string resultado = nomes.Find(VerificaNomeNaLista);
+//Utilizando método anônimo
+string resultado = nomes.Find(nome => nome.Equals("Eduardo"));
+
+Console.WriteLine(resultado);
+
+
+
+
+Console.ReadKey();
+
+
